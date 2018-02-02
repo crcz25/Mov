fef0a2b6779519a05a3b24520625d8a07e01c926
crcz <A01209474@itesm.mx>
2018-02-02 15:27:22 -0600

    finish harcoded login

--- Lab3/app/src/main/java/com/example/crcz/lab3/Main2Activity.java
+++ Lab3/app/src/main/java/com/example/crcz/lab3/Main2Activity.java
@@ -1,7 +1,12 @@
 package com.example.crcz.lab3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Bundle bundle = getIntent().getExtras();
+
+        String data = bundle.getString("data");
+
+        TextView myText = (TextView) findViewById(R.id.textView1);
+        if(data.length() > 0){
+            myText.setText(data);
+
+        }
+
     }
 }
--- Lab3/app/src/main/java/com/example/crcz/lab3/MainActivity.java
+++ Lab3/app/src/main/java/com/example/crcz/lab3/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.crcz.lab3;
 
+import android.app.AlertDialog;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
@@ -20,12 +23,23 @@ public class MainActivity extends AppCompatActivity {
     EditText ed1, ed2;
 
     TextView txt;
+    String mytext;
+    Context context;
 
+    public void passData(View view, String userName){
+        String text = "Welcome " + userName;
+
+        Intent pass_data = new Intent(MainActivity.this, Main2Activity.class);
+        pass_data.putExtra("data",text);
+        startActivity(pass_data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = getApplicationContext();
+
 
         bLogin = (Button)findViewById(R.id.button1);
         ed1 = (EditText)findViewById(R.id.editText);
@@ -35,10 +49,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(ed1.getText().toString().equals("admin") && ed2.getText().toString().equals("admin")) {
-                    Toast.makeText(getApplicationContext(), "Redirecting...",Toast.LENGTH_SHORT).show();
-
-                    startActivity(new Intent(MainActivity.this, Main2Activity.class).putExtra("HOLI","Welcome"));
+                    Toast.makeText(getApplicationContext(), "Password OK!",Toast.LENGTH_SHORT).show();
+                    passData(v, ed1.getText().toString());
                 }else{
+                    AlertDialog.Builder SecondDialogConf = new AlertDialog.Builder(MainActivity.this);
+
+                    SecondDialogConf.setTitle("Confirmation Dialog");
+                    SecondDialogConf.setMessage("Wrong!!");
+                    SecondDialogConf.setIcon(R.mipmap.ic_launcher);
+
+                    SecondDialogConf.setPositiveButton("Retry",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    Toast.makeText(context, "You answer Retry.", Toast.LENGTH_LONG).show();
+                                    finish();
+                                    startActivity(getIntent());
+                                }
+                            });
+
+                    AlertDialog MySecondDialog = SecondDialogConf.create();
+                    MySecondDialog.show();
+
                     Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
                 }
             }
